ff45c231af28e99c64428de6ec8377e5fcdedeee
Vincent Sidot <v.sidot@aniah.fr>
2025-08-21 16:10:08 +0200

    [WIP] font loading

--- src/rasterizer/font.rs
+++ src/rasterizer/font.rs
@@ -13,31 +13,19 @@ use std::fs;
 #[cfg(feature = "rasterizer")]
 const DEFAULT_FONT: &[u8] = include_bytes!("../../font/ubuntu.ttf");
 
-/// A font that can be used to render text
+#[cfg(feature = "rasterizer")]
 #[derive(Debug)]
-pub struct FontRenderer<'f> {
-    #[cfg(feature = "rasterizer")]
+struct FontCow<'f> {
     _font_data: Cow<'f, [u8]>,
-    #[cfg(feature = "rasterizer")]
     font: FontRef<'f>,
-    #[cfg(feature = "rasterizer")]
-    scale: PxScale,
 }
 
-impl<'f> FontRenderer<'f> {
-    /// Create a new font renderer with the specified font data and size
-    #[cfg(feature = "rasterizer")]
-    pub fn new<F, P>(font_data: Option<F>, size: f32) -> Result<Self, Box<dyn std::error::Error>>
+impl<'f> FontCow<'f> {
+    fn init<F, P>(font_data: Option<F>) -> Result<Self, Box<dyn std::error::Error>>
     where
         P: AsRef<Path> + 'f,
         F: Into<FontSource<'f, P>>,
     {
-        // let font_source = font_data.into();
-        // let font_bytes: Cow<'f, [u8]> = match font_source {
-        //     FontSource::Bytes(bytes) => Cow::Borrowed(bytes),
-        //     FontSource::Path(path) => Cow::Owned(fs::read(path)?),
-        // };
-
         let font_data: Cow<'f, [u8]> = font_data
             .map(|data| match data.into() {
                 FontSource::Bytes(bytes) => Cow::Borrowed(bytes),
@@ -60,28 +48,49 @@ impl<'f> FontRenderer<'f> {
             core::mem::transmute_copy(&font_data.as_ref())
         };
 
-        // For now, we'll just use the default font since we can't easily make
-        // the loaded font data live long enough for FontRef
-        // In a real implementation, you'd use owned_ttf_parser or similar
-        let font = FontRef::try_from_slice(leaked_font_data)?;
-        let scale = PxScale::from(size);
+        let font: FontRef<'f> = FontRef::try_from_slice(leaked_font_data)?;
         Ok(Self {
             _font_data: font_data,
             font,
-            scale,
         })
+    }
+
+    pub fn default() -> Result<Self, Box<dyn std::error::Error>> {
+        let font = FontRef::try_from_slice(DEFAULT_FONT)?;
+        Ok(Self {
+            font,
+            _font_data: Cow::Borrowed(DEFAULT_FONT),
+        })
+    }
+
+    pub fn font(&self) -> &FontRef<'f> {
+        &self.font
+    }
+}
+
+/// A font that can be used to render text
+#[derive(Debug)]
+pub struct FontRenderer<'f> {
+    #[cfg(feature = "rasterizer")]
+    font: FontRef<'f>,
+    #[cfg(feature = "rasterizer")]
+    scale: PxScale,
+}
+
+impl<'f> FontRenderer<'f> {
+    /// Create a new font renderer with the specified font data and size
+    #[cfg(feature = "rasterizer")]
+    pub fn new(font_data: Option<&'f [u8]>, size: f32) -> Result<Self, Box<dyn std::error::Error>> {
+        let font_data = font_data.unwrap_or(DEFAULT_FONT);
+        let font = FontRef::try_from_slice(font_data)?;
+        let scale = PxScale::from(size);
+        Ok(Self { font, scale })
     }
 
     /// Create a new font renderer with a default font (Ubuntu) and size
     #[cfg(feature = "rasterizer")]
     pub fn default(size: f32) -> Result<Self, Box<dyn std::error::Error>> {
-        let font = FontRef::try_from_slice(DEFAULT_FONT)?;
-        let scale = PxScale::from(size);
-        Ok(Self {
-            font,
-            scale,
-            _font_data: Cow::Borrowed(DEFAULT_FONT),
-        })
+        Self::new(None, size)
     }
 
     /// Rasterize text onto an image at the specified position
@@ -231,29 +240,5 @@ impl FontRenderer {
     /// Calculate the width and height of the text when rendered
     pub fn measure_text(&self, _text: &str) -> (f32, f32) {
         (0.0, 0.0)
-    }
-}
-
-/// Source for font data
-#[derive(Debug)]
-pub enum FontSource<'f, P: AsRef<Path>> {
-    /// Font data as bytes
-    Bytes(&'f [u8]),
-    /// Path to a font file
-    Path(&'f P),
-}
-
-impl<'f, P: AsRef<Path>> From<&'f [u8]> for FontSource<'f, P> {
-    fn from(bytes: &'f [u8]) -> Self {
-        Self::Bytes(bytes)
-    }
-}
-
-impl<'f, P> From<&'f P> for FontSource<'f, P>
-where
-    P: AsRef<Path>,
-{
-    fn from(path: &'f P) -> Self {
-        Self::Path(path)
     }
 }
